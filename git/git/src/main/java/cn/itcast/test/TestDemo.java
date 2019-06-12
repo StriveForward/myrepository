0d78a1959e7c0048015001e33d5069bec5f3f1c9
Mr.HE <1241738794@qq.com>
2019-06-12 17:40:32 +0800

    update

--- git/git/src/main/java/cn/itcast/test/TestDemo.java
+++ git/git/src/main/java/cn/itcast/test/TestDemo.java
@@ -3,9 +3,13 @@ package cn.itcast.test;
 public class TestDemo {
 
     public static void main(String[] args) {
-        for (int i = 0; i < 10; i++) {
+        for (int i = 1; i <= 10; i++) {
             System.out.println("报数: "+i);
         }
+    }
+
+    public void test(){
+        System.out.println("upload first project");
     }
 
 }
