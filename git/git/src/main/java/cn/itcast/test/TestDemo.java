2ab7d91b7de8e294a627ed74319ff5807b6fc75a
Mr.HE <1241738794@qq.com>
2019-06-12 16:47:13 +0800

    FirstProject commit

---
+++ git/git/src/main/java/cn/itcast/test/TestDemo.java
@@ -0,0 +1,11 @@
+package cn.itcast.test;
+
+public class TestDemo {
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            System.out.println("报数: "+i);
+        }
+    }
+
+}
